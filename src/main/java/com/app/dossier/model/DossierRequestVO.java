d6faa39074076b3915f44c2ea051b82197ba1460
sraivante <118155042+sraivante@users.noreply.github.com>
2026-02-20 21:00:30 +0530

    Add/update src/main/java/com/app/dossier/model/DossierRequestVO.java

---
+++ src/main/java/com/app/dossier/model/DossierRequestVO.java
@@ -0,0 +1,24 @@
+package com.app.dossier.model;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class DossierRequestVO {
+    private PersonDossier personDossier;
+    private List<Associate> associate;
+    private List<FamilyDetail> familyDetail;
+
+    //mahila mitra sasural ka vivran
+    private List<LegalContact> legalContact;
+    private List<CoAccused> coAccused;
+    private List<CriminalHistory> criminalHistory;
+    private List<CrimeVehicle> crimeVehicle;
+    private List<CrimeWeapon> crimeWeapon;
+    private List<CrimeProperty> crimeProperty;
+    private List<FamilyCompany> familyCompany;
+    private List<IdentifyingOfficer> identifyingOfficer;
+    private List<NewBusiness> newBusiness;
+
+}
